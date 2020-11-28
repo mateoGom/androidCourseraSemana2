c52798bb6a722ced27f4a8012bb0a0f380afc6d2
mateoGom <matemgm@hotmail.com>
2020-11-28 14:07:20 +0100

    adding swipe refresh layout

--- app/src/main/java/com/example/tareasemana2/MainActivity.java
+++ app/src/main/java/com/example/tareasemana2/MainActivity.java
@@ -1,15 +1,23 @@
 package com.example.tareasemana2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    SwipeRefreshLayout srLayout;
+    ListView lvList;
+    Adapter adaptador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +25,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         miFab();
 
+        srLayout=  findViewById(R.id.srLayout);
+        lvList = findViewById(R.id.lvList);
+        String[] planetas= getResources().getStringArray(R.array.planetas);
+        lvList.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1,planetas));
+        srLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refreshContent();
+            }
+        });
     }
+
+public void refreshContent(){
+    String[] planetas= getResources().getStringArray(R.array.planetas2);
+    lvList.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1,planetas));
+    srLayout.setRefreshing(false);
+}
+
+
+
+
 
 public void miFab(){
     FloatingActionButton fab = findViewById(R.id.fab_airballoon);
